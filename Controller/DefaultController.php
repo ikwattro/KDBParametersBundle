4966a7b47f1e2e8e18abe85c08d47c04a1cca85e
Christophe Willemsen <willemsen.christophe@gmail.com>
2011-12-28 16:54:07 +0100

    removed dependencies in Controller, now extending ContainerAware

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,22 +2,30 @@
 
 namespace KDB\ParametersBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 use KDB\ParametersBundle\Form\ParameterFormType;
 
 
-class DefaultController extends Controller
+class DefaultController extends ContainerAware
 {
     
-    public function indexAction($name)
+    public function indexAction()
     {
         //Create the params form
         $class = $this->container->getParameter('kdb_parameters.class');
         $param = new $class;
         
-        $form = $this->createForm(new ParameterFormType($class), $param);
+        $form = $this->container->get('form.factory')->create(new ParameterFormType($class), $param, array());
         
-        return $this->render('KDBParametersBundle:Default:index.html.twig', array('name' => $name, 'form' => $form->createView()));
+        return $this->container->get('templating')->renderResponse('KDBParametersBundle:Default:index.html.twig',array(
+            'form' => $form->createView(),
+        ));
+        
+        //return $this->render('KDBParametersBundle:Default:index.html.twig', array('name' => $name, 'form' => $form->createView()));
     }
 }
