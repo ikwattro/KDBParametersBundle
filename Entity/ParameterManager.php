2350aecf198a092feab942b05c6c7c1afc26124a
Christophe Willemsen <willemsen.christophe@gmail.com>
2011-12-29 11:00:06 +0100

    fixed some licenses issues

--- Entity/ParameterManager.php
+++ Entity/ParameterManager.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of the FOSUserBundle package.
+ * This file is part of the KDBParametersBundle package.
  *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ * (c) kwattro <https://github.com/kwattro/>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- KDBParametersBundle.php
+++ KDBParametersBundle.php
@@ -1,9 +1,22 @@
 <?php
 
+/*
+ * This file is part of the KDBParametersBundle package.
+ *
+ * (c) kwattro <https://github.com/kwattro/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace KDB\ParametersBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * @package KDBParametersBundle
+ * @author Christophe Willemsen <willemsen.christophe@gmail.com/>
+ */
 class KDBParametersBundle extends Bundle
 {
 }
